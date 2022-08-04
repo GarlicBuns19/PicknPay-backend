54a553b10763fb5783d7fc6085a2c4bc53b62c99
GarlicBuns19 <planterx26@gmail.com>
2022-08-04 12:16:32 +0200

    Update

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ const express = require('express')
 const app = express()
 const db = require('./config/dbmysql')
 const cors = require('cors')
-const user = require('./model/user')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const router = express.Router();
 // Set up port
 const port = process.env.PORT || 5001
 
-app.use(router, express.json(), user, express.urlencoded({
+app.use(router, express.json(), express.urlencoded({
     extended: true
 }))
 app.use(cors())
 
+// Home
+app.get('/',(req,res) => {
+    res.send('Lets go my g')
+})
+
+// Register
+app.post('/reg', bodyParser.json(), (req, res) => {
+    let bd = req.body;
+    let sql = `INSERT into users (userFName, userLName ,userEmail ,userPassword ,userRole)
+    VALUES(?, ?, ?, ?, ?);`;
+    db.query(sql, [bd.userFName, bd.userLName, bd.userEmail, bd.userPassword, bd.userRole],
+        (err, results) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(`number rows affected ${results}`)
+            }
+        }
+    )
+});
 // const router = require(express.router())
 // app.use('/api')
 
