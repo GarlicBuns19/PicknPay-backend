475c3bcef5de676a299366e27ea5e43313b56af2
GarlicBuns19 <planterx26@gmail.com>
2022-08-04 11:42:38 +0200

    Update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const db = require('./config/dbmysql')
 const cors = require('cors')
-
+const user = require('./model/user')
 // Set up port
 const port = process.env.PORT || 5001
 
-app.use(express.json())
+app.use(express.json(), user)
 app.use(cors())
 
 // const router = require(express.router())
