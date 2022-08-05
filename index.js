782440163e6930083a49b7b86b5a71754ae515e4
GarlicBuns19 <planterx26@gmail.com>
2022-08-05 11:26:14 +0200

    Update

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const path = require('path');
 const bcrypt = require('bcrypt')
-const { json } = require('body-parser')
+const {
+    json
+} = require('body-parser')
 const router = express.Router();
 // Set up port
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5002
 
 app.use(router, express.json(), express.urlencoded({
     extended: true
@@ -17,11 +19,20 @@ app.use(router, express.json(), express.urlencoded({
 app.use(cors())
 
 // Home
-app.get('/',(req,res, next) => {
-    res.sendFile('./index.html', {root : __dirname})
+app.get('/', (req, res, next) => {
+    res.sendFile('./index.html', {
+        root: __dirname
+    })
 })
-app.get('/prods',(req,res, next) => {
-    res.sendFile('./prod.html', {root : __dirname})
+app.get('/prods', (req, res, next) => {
+    res.sendFile('./prod.html', {
+        root: __dirname
+    })
+})
+app.get('/signup', (req, res, next) => {
+    res.sendFile('./login.html', {
+        root: __dirname
+    })
 })
 
 // Register
@@ -38,38 +49,89 @@ app.post('/reg', bodyParser.json(), async (req, res) => {
                 res.send(`number rows affected ${results.affectedRows}`)
             }
         }
-        )
-        res.redirect('/')
+    )
+    res.redirect('/')
 });
 
-app.post('/login',bodyParser.json(),(req,res) =>{
-    try{
-        const {userEmail,userPassword} = req.body;
-        const sql = `SELECT userFName,userlName,userEmail,userPassword
-        FROM users
-        WHERE userEmail = ${userEmail};`;
-        db.query(sql,async (err,results)=> {
-            if(err){
-                console.log(err)
-            }else{
+// app.post('/login',bodyParser.json(),async (req,res) =>{
+//     try{
+//         const {userEmail,userPassword} = req.body;
+//         const sql = `SELECT userFName,userlName,userEmail,userPassword
+//         FROM users
+//         WHERE userEmail = ${userEmail};`;
+//         db.query(sql,async (err,results)=> {
+//             switch(true){
+//                 case(await bcrypt.compare(userPassword,results[0].userPassword)):
+//                 res.send('U loged in')
+//                 break
+//                 default:
+//                     console.log('See u')
+//             }
+//         })
+//     }catch{}
+// })
 
+app.post('/login', bodyParser.json(), (req, res) => {
+    const strQry = `SELECT * FROM users WHERE ? ;`;
+    let user = {
+        userEmail: req.body.userEmail
+    };
+    db.query(strQry, user, async (err, results) => {
+        if (err) throw err;
+        if (results.length === 0) {
+            res.send('Email not found. Please register')
+        } else {
+            const isMatch = await bcrypt.compare(req.body.userPassword, results[0].userPassword);
+            if (!isMatch) {
+                res.send('Password is Incorrect')
+            } else {
+                const payload = {
+                    user: {
+                        userFName: results[0].userFName,
+                        userLName: results[0].userLName,
+                        userRole: results[0].userRole,
+                        userEmail: results[0].userEmail,
+                        userPassword: results[0].userPassword,
+                    },
+                };
+                jwt.sign(payload, process.env.jwtsecret, {
+                    expiresIn: "365d"
+                }, (err, token) => {
+                    if (err) throw err;
+                    res.send(token)
+                });
             }
-        })
-    }catch{}
+        }
+    })
 })
 
-app.get('/prod',bodyParser.json(),(req,res) => {
-    let bd= req.body
+app.get("/users/verify", (req, res) => {
+    const token = req.header("x-auth-token");
+    jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).send("Unauthorized Access!");
+      } else {
+        res.status(200).send(decodedToken);
+      }
+    });
+  });
+
+app.get('/prod', bodyParser.json(), (req, res) => {
+    let bd = req.body
     let sql = `SELECT * FROM products`
-    db.query(sql,(err,results) => {
-        if(err){
+    db.query(sql, (err, results) => {
+        if (err) {
             console.log(err)
-        }else{
-            res.json({results : results})
+        } else {
+            res.json({
+                results: results
+            })
         }
     })
 })
 // const router = require(express.router())
 // app.use('/api')
 
-app.listen(port , () => {console.log(`Sever is running http://localhost:${port}`)})
+app.listen(port, () => {
+    console.log(`Sever is running http://localhost:${port}`)
+})
